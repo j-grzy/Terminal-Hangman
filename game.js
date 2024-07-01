edf4c31fa343396f8ffefdfb31b1043bbf0dedc2
j-grzy <jana_grzywacz@web.de>
2024-07-01 11:43:40 +0200

    Initital Commit

---
+++ game.js
@@ -0,0 +1,12 @@
+const gradient = require("gradient-string");
+/* const { QMainWindow, QWidget, FlexLayout, QLabel, QPushButton, QLineEdit } = require("@nodegui/nodegui");
+const win = new QMainWindow();
+win.resize(1000, 800);
+const centralWidget = new QWidget();
+const rootLayout = new FlexLayout;
+centralWidget.setLayout(rootLayout);
+//rootLayout.addWidget(button);
+//rootLayout.addWidget(input);
+win.setCentralWidget(centralWidget);
+win.show();
+global.win = win;  */
