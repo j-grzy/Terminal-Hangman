8f4aa8d537a7bf3e013e206ed466881605feae61
j-grzy <jana_grzywacz@web.de>
2024-07-01 12:35:06 +0200

    basic setup

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 import rls from "readline-sync";
 import gradient from "gradient-string";
 
+const user = {
+    name: ""
+};
+const menu = [];
 const titles = {
     hangman: gradient("yellow", "orange", "yellow", "orange", "yellow").multiline(
 `
@@ -12,5 +16,21 @@ const titles = {
 ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
 `)
 }
+const hangman = {
+    title: titles.hangman
+}
 
+function welcomeUser(){
+    console.log(`Welcome!\nWhat's your name?`);
+    let name = rls.question("> ");
+    user.name = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    console.log("Hello", user.name);
+    startGame();
+}
 
+function startGame(){
+    function newRound(){
+    }
+}
+
+welcomeUser();
