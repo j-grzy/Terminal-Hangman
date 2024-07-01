13add84520c75cf819e54e6d78ffffbb381357c0
j-grzy <jana_grzywacz@web.de>
2024-07-01 12:06:59 +0200

    remove nodegui

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
-const gradient = require("gradient-string");
-/* const { QMainWindow, QWidget, FlexLayout, QLabel, QPushButton, QLineEdit } = require("@nodegui/nodegui");
-const win = new QMainWindow();
-win.resize(1000, 800);
-const centralWidget = new QWidget();
-const rootLayout = new FlexLayout;
-centralWidget.setLayout(rootLayout);
-//rootLayout.addWidget(button);
-//rootLayout.addWidget(input);
-win.setCentralWidget(centralWidget);
-win.show();
-global.win = win;  */
+import rls from "readline-sync";
+import gradient from "gradient-string";
+
+const titles = {
+    hangman: gradient("yellow", "orange", "yellow", "orange", "yellow").multiline(
+`
+██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗
+██║  ██║██╔══██╗████╗  ██║██╔════╝ ████╗ ████║██╔══██╗████╗  ██║
+███████║███████║██╔██╗ ██║██║  ███╗██╔████╔██║███████║██╔██╗ ██║
+██╔══██║██╔══██║██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║
+██║  ██║██║  ██║██║ ╚████║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+`)
+}
+
+
